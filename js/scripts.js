e0788c4d63dc514851f90c2c34c97fbc8b4d894d
Ashley Berry <ashleywberry@gmail.com>
2020-11-01 10:34:00 -0600

    video is go

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,13 @@
     }
   });
 
+  $(document).ready(function() {
+    $('#headerVideoLink').magnificPopup({
+      type:'inline',
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });         
+  });
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
